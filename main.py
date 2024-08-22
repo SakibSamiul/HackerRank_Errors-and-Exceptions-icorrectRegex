92a8021cc7f31c880545d83828081e8d09b1019d
SakibSamiul <sb.sakib77@gmail.com>
2024-08-22 19:35:43 +0600

    Regex

---
+++ main.py
@@ -0,0 +1,16 @@
+import re
+
+T = int(input().strip())
+
+for _ in range(T):
+    S = input().strip()
+
+    if any(op + '+' in S for op in ['*', '+', '?']):
+        print("False")
+    else:
+        try:
+            re.compile(S)
+            print("True")
+        except re.error:
+            print("False")        
+
